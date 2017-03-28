cd8f5a8e193041f20993aa0d9b7371727eddf269
Marcin Krolik <marcin.krolik@intel.com>
2017-03-28 22:28:03 +0200

    Initial version of slack slack publihser

---
+++ README.md
@@ -0,0 +1,62 @@
+# Snap publisher plugin - Slack
+
+This plugin supports pushing metrics to Slack via webhooks.
+
+It's used in the [Snap framework](http://github.com/intelsdi-x/snap).
+
+1. [Getting Started](#getting-started)
+  * [System Requirements](#system-requirements)
+  * [Configuration and Usage](#configuration-and-usage)
+2. [Documentation](#documentation)
+  * [Collected Metrics](#collected-metrics)
+  * [Examples](#examples)
+  * [Roadmap](#roadmap)
+3. [Community Support](#community-support)
+4. [Contributing](#contributing)
+5. [License](#license)
+6. [Acknowledgements](#acknowledgements)
+
+## Getting Started
+
+### System Requirements
+
+* [golang 1.6+](https://golang.org/dl/) (needed only for building)
+
+#### To build the plugin binary:
+Fork https://github.com/marcin-krolik/snap-plugin-publisher-slack
+
+Clone repo into `$GOPATH/src/`:
+
+```
+$ git clone https://github.com/<yourGithubID>/snap-plugin-publisher-slack.git
+```
+
+Build the plugin by running make within the cloned repo:
+```
+$ make
+```
+This builds the plugin in `./build`
+
+### Configuration and Usage
+* Set up the [Snap framework](https://github.com/intelsdi-x/snap/blob/master/README.md#getting-started)
+
+## Documentation
+
+The plugin expects you to provide the following parameters:
+ - `webook`
+ - `publish_fields`
+ - `channel`
+ - `username` (optional)
+
+### Examples
+
+TBD
+
+### Roadmap
+
+TBD
+
+## Acknowledgements
+* Author: [Marcin Krolik](https://github.com/marcin-krolik)
+
+And **thank you!** Your contribution, through code and participation, is incredibly important to us.
---
+++ slack/slack.go
@@ -0,0 +1,100 @@
+/*
+http://www.apache.org/licenses/LICENSE-2.0.txt
+
+
+Copyright 2017 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package slack
+
+import (
+	"encoding/json"
+	"net/http"
+	"bytes"
+	"strings"
+
+	"github.com/intelsdi-x/snap-plugin-lib-go/v1/plugin"
+	reflect "gopkg.in/oleiade/reflections.v1"
+)
+
+const (
+	Name = "slack"
+	Version = 1
+)
+
+//NewSlackPublisher returns an instance of the Slack publisher
+func NewPublisher() plugin.Publisher {
+	return &slackPublisher{}
+}
+
+//GetConfigPolicy returns plugins config policy
+func (p *slackPublisher) GetConfigPolicy() (plugin.ConfigPolicy, error) {
+	policy := plugin.NewConfigPolicy()
+	policy.AddNewStringRule([]string{""}, "webhook", true)
+	policy.AddNewStringRule([]string{""}, "channel", true)
+	policy.AddNewStringRule([]string{""}, "username", false, plugin.SetDefaultString("webhookbot"))
+	policy.AddNewStringRule([]string{""}, "publish_fields", false, plugin.SetDefaultString("Namespace|Config|Data|Description|Timestamp|Unit|Version"))
+
+	return *policy, nil
+}
+
+// Publish publishes metric data to slack
+func (p *slackPublisher) Publish(metrics []plugin.Metric, config plugin.Config) error {
+	url, err := config.GetString("webhook")
+	if err != nil {
+		return err
+	}
+	publish_fields, err := config.GetString("publish_fields")
+	if err != nil {
+		return err
+	}
+	//TODO: current implementation publishes each metric separately; add publish as group (consider max message size)
+	for _, metric := range metrics {
+		toPublish := map[string]interface{}{}
+		//TODO: validation of publish_field format to avoid errors after split string
+		for _, field := range strings.Split(publish_fields, "|") {
+			var value interface{}
+			value, err = reflect.GetField(metric, field)
+			if err != nil {
+				return err
+			}
+			if field == "Namespace" {
+				value = strings.Join(metric.Namespace.Strings(), "/")
+			}
+			toPublish[field] = value
+		}
+		message, err := json.Marshal(toPublish)
+		if err != nil {
+			return err
+		}
+		payload := map[string]string{
+			"text": string(message),
+			"channel": "#general",
+			"username": "monkey-bot",
+		}
+		js, err := json.Marshal(payload)
+		if err != nil {
+			return err
+		}
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(js))
+		if err != nil {
+			return err
+		}
+		resp.Body.Close()
+	}
+	return nil
+}
+
+type slackPublisher struct{}
